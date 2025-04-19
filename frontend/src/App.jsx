970e5a0b752244f839e1fda8406d932aff770180
HarshitRaj-123 <harshitrajhr710@gmail.com>
2025-04-19 21:49:34 +0530

    Initial commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <p class="text-7xl font-bold underline">
+        Hello world!
+      </p>
+    </>
+  )
+}
+
+export default App
